ecba074351040dc129499832b026b7238cbe9bdf
Luís Freitas <luiseugg@gmail.com>
2026-07-16 15:04:05 -0300

    feat: readline interface

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,19 @@
 import fs from 'fs';
+import readline from 'readline';
+
+const rl = Readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> ',
+});
+
+while(true){
+    console.log("1. Add task")
+    console.log("2. Update task")
+    console.log("3. Delete task")
+    console.log("4. List tasks")
+    const input = prompt();
+}
 
 function addTask() {
     fs.readFile('./bin/tasks/tasks.json', function (err, data) {
@@ -26,29 +41,28 @@ function updateTask() {
     fs.readFile('./bin/tasks/tasks.json', function (err, data) {
         if (err) throw err;
         const jsonData = JSON.parse(data.toString());
-        jsonData.todo.splice(2, 2, "node")
+        jsonData.todo.splice(index, 1, updatedTask)
         fs.writeFile("./bin/tasks/tasks.json", JSON.stringify(jsonData), 'utf8', (err) => {
             if (err) {
                 console.error(err);
             } else {
-                console.log("Task added successfully");
+                console.log("Task updated successfully");
             }
         })
     })
 }
-/*
-function deleteTask() {
+
+function deleteTask(index) {
     fs.readFile('./bin/tasks/tasks.json', function (err, data) {
         if (err) throw err;
         const jsonData = JSON.parse(data.toString());
-        jsonData.todo.splice(2, 2)
+        jsonData.todo.splice(index, 1)
         fs.writeFile("./bin/tasks/tasks.json", JSON.stringify(jsonData), 'utf8', (err) => {
             if (err) {
                 console.error(err);
             } else {
-                console.log("Task added successfully");
+                console.log("Task deleted successfully");
             }
         })
     })
-}*/
-addTask();
+}
